9f264f3f3fb842345e06f2aa79c15f013b87a269
Anushree GL <anushreegl007@gmail.com>
2026-09-16 18:02:17 +0530

    Add Valid Anagram solution

---
+++ arrays-strings/03-valid-anagram.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    int count[256] = {0};
+    int i;
+    int isAnagram = 1;
+
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    if (strlen(str1) != strlen(str2))
+    {
+        isAnagram = 0;
+    }
+    else
+    {
+        for (i = 0; str1[i] != '\0'; i++)
+        {
+            count[(unsigned char)str1[i]]++;
+            count[(unsigned char)str2[i]]--;
+        }
+
+        for (i = 0; i < 256; i++)
+        {
+            if (count[i] != 0)
+            {
+                isAnagram = 0;
+                break;
+            }
+        }
+    }
+
+    if (isAnagram)
+        printf("The strings are anagrams.\n");
+    else
+        printf("The strings are not anagrams.\n");
+
+    return 0;
+}
